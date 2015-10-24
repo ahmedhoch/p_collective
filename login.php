1359ddbce227b2785572e0d30fbf92fd677ee945
ahmedhoch <abdourahmanhoche@outlook.fr>
2015-10-24 19:08:13 +0200

    modification complet

---
+++ login.php
@@ -0,0 +1,17 @@
+<html xmlns="http://www.w3.org/1999/html">
+<meta>
+
+</meta>
+<head>
+    <title>salut ceci est mon premier entrainement
+
+    </title>
+</head>
+<body>
+<div>
+  <h1>
+      BIENVENU
+  </h1>
+</div>
+</body>
+</html>echo "salut"
